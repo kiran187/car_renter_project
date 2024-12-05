88904934c533a8d10a45c188f80f88f723e94863
kiran187 <kirantupe1112@gmail.com>
2024-12-05 14:18:51 +0530

    Update conection.js

--- router/conection.js
+++ router/conection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    "host":"localhost",
-    "user":"root",
-    "password":"",
-    "database":"car_rental_project"
+    "host":"btqg8tqnrwdd0lc4s8ph-mysql.services.clever-cloud.com",
+    "user":"uwnlq1uln6ddx5sz",
+    "password":"5os7ZIEFqOkbBUCj9j4X",
+    "database":"btqg8tqnrwdd0lc4s8ph"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
